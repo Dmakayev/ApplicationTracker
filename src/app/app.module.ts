44f06ea5f5da9cffc6615d5efb842458b1bf388c
Dmakayev <dimitri.makayev@gmail.com>
2022-07-26 21:37:46 -0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ToDoItemsComponent } from './components/to-do-items/to-do-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ToDoItemsComponent
   ],
   imports: [
     BrowserModule
