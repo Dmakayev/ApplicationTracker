ecabac59f01d4f18b400172a2adfb4ee9e7994a2
Dmakayev <dimitri.makayev@gmail.com>
2022-08-05 18:46:53 -0500

    2. Connected Firebase DB Working Create, Read

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyApSSED2H5B1SIu_u-geDmyFCY2xrL322A",
+    authDomain: "application-tracker-ca704.firebaseapp.com",
+    projectId: "application-tracker-ca704",
+    storageBucket: "application-tracker-ca704.appspot.com",
+    messagingSenderId: "632158486737",
+    appId: "1:632158486737:web:b547fc3f7d3a36217ec5f0",
+    measurementId: "G-CBKNY84E1N"
+  }
 };
 
 /*
